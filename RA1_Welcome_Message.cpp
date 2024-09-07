e1c003c901fe465c1feeea92134c6a07d52511a4
Marquise PC <marzphillip@gmail.com>
2024-09-07 17:48:25 -0400

    RA1 Welcome message completed

---
+++ RA1_Welcome_Message.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Marquise on 9/7/2024.
+//
+
+// Name: Welcome Message
+// Description: Welcomes a student to CS280
+
+#include "RA1_Welcome_Message.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    string firstName;
+    cout << "Please enter your first name >>" << endl;
+    cin >> firstName;
+    string lastName;
+    cout << "Please enter your last name >>" << endl;
+    cin >> lastName;
+    int sectionNumber;
+    cout << "Please enter your CS 280 section number >>" << endl;
+    cin >> sectionNumber;
+
+    cout << "Welcome " << firstName << " to CS 280 section " << sectionNumber << endl;
+
+
+
+
+    return 0;
+}
---
+++ RA1_Welcome_Message.h
@@ -0,0 +1,16 @@
+//
+// Created by Marquise on 9/7/2024.
+//
+
+#ifndef RA1_WELCOME_MESSAGE_H
+#define RA1_WELCOME_MESSAGE_H
+
+
+
+class RA1_Welcome_Message {
+
+};
+
+
+
+#endif //RA1_WELCOME_MESSAGE_H
